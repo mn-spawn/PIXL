a0b091cf053edddd7bf3b4cdc0baae91117282a9
dor-web <dor-web@users.noreply.github.com>
2022-10-09 01:06:40 -0700

    Update ReviewState.js
    clean up ReviewState.js

--- src/components/ReviewState.js
+++ src/components/ReviewState.js
@@ -1,13 +1,13 @@
-import { render } from '@testing-library/react';
 import React, {useState}  from 'react'
 import TodoList from './TodoList';
 import QRCode from 'qrcode';
-import { useLocation } from 'react-router-dom'
 import {data} from "./TodoList";
+
 
 
 function ReviewState() {
   console.log({data})
+
   const[qrcode, setQrcode] = useState('')
   const CreateQRCode = () =>{
     QRCode.toDataURL('http://localhost:3000/review',{
@@ -20,8 +20,9 @@ function ReviewState() {
     return (
       <div>
         <h1>QR Code</h1>
+
         {data.map(data=> <ul>{data.text}</ul>)}
-        <button onClick={CreateQRCode}><script src='url_links.js'/>Generate</button>
+        <button onClick={CreateQRCode}>Generate</button>
         <br></br>
         {qrcode && <> 
         <img src={qrcode}/>
