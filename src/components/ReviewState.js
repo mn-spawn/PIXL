bfb43152e6a26f562d032a9576c4e006b0d854d7
dor-web <dor-web@users.noreply.github.com>
2022-10-08 20:48:02 -0700

    Add QR to review state
    include the QR generate code, download image, and the "npm install  qrcode"

--- src/components/ReviewState.js
+++ src/components/ReviewState.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
+import QRCode from 'qrcode';
+// import './src/App.css';
 
 function ReviewState({parentToChild}) {
+
+    const[qrcode, setQrcode] = useState('')
+
+    const CreateQRCode = () =>{
+        QRCode.toDataURL('http://localhost:3000/review',{
+        width: 500,
+        margin: 3
+        },(err,url) => {
+        setQrcode(url)
+        })
+    }
         return (
             <div>
                 <h1>This is review state</h1>
                 {parentToChild}
+                <button onClick={CreateQRCode}>Generate</button>
+                <br></br>
+                {qrcode && <React.Fragment> 
+                    <img src={qrcode}/>
+                    <br></br>     
+                    <a href={qrcode} download="qrcode.png"> Download</a>
+                </React.Fragment>}
             </div>
           )
 }
